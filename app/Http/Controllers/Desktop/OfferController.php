b416d94fbdee70f2103e17103d27146d5dbc44b1
A-Dev <3xasov@gmail.com>
2023-06-20 10:44:07 +0800

    update offer id

--- app/Http/Controllers/Desktop/OfferController.php
+++ app/Http/Controllers/Desktop/OfferController.php
@@ -16,7 +16,7 @@ class OfferController extends Controller
      */
     public function index()
     {
-        $setting = Page::where('is_active', '1')->where('id', '15')->first();
+        $setting = Page::where('is_active', '1')->where('id', '17')->first();
         $offer_list = Offer::where('is_active', '1')->latest()->get();
         return view('seven_secret.offer')->with(compact('setting', 'offer_list'));
     }
