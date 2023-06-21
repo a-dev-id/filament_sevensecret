5e6f974699de1e38b29ba6a6e68bb2ee5bb388a5
A-Dev <3xasov@gmail.com>
2023-06-21 15:04:52 +0800

    contact update

--- resources/views/seven_secret_mobile/contact.blade.php
+++ resources/views/seven_secret_mobile/contact.blade.php
@@ -35,7 +35,7 @@
         <div class="container-fluid contact py-2">
             <div class="row">
                 <div class="col-12">
-                    <img src="{{asset('images/logo-sevensecret.png')}}" alt="{{config('app.name')}} - Logo" class="img-logo my-3" style="width:70%">
+                    <img src="{{asset('images/logo-sevensecret.png')}}" alt="{{config('app.name')}} - Logo" class="img-logo my-3" style="width:50%">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4152.808820475982!2d116.04592373372977!3d-8.428285441811916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcddd537ea4ceb5%3A0xde92945bc8e915e0!2sSeven%20Secrets%20Resort%20by%20Hanging%20Gardens!5e1!3m2!1sen!2sid!4v1686715753101!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     <ul class="list-unstyled">
                         <li class="my-1"><i class="fa fa-map-marker me-1"></i> Jl. Raya Pemenang No.KM 10, Malaka, Kec. Pemenang, Kab. Lombok Utara, Nusa Tenggara Barat, Indonesia</li>
